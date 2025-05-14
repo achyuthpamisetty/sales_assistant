39390f035a0ba83f0a0d2b11b54294d4d5cbd252
achyuthpamisetty <50010594+achyuthpamisetty@users.noreply.github.com>
2025-05-14 15:41:39 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// Vite configuration
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'),  // Alias for '@' to map to './src'
     },
   },
 });
