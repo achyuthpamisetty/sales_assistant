0381bcfcebe9281142b379859dcbbaa66cf96a74
achyuthpamisetty <50010594+achyuthpamisetty@users.noreply.github.com>
2025-05-15 13:49:51 +0530

    Updated supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 // src/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://YOUR_PROJECT_ID.supabase.co';
-const supabaseAnonKey = 'YOUR_ANON_KEY';
+const supabaseUrl = 'https://vqdqcgzbnbwusedlmere.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxZHFjZ3pibmJ3dXNlZGxtZXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2ODY0MjcsImV4cCI6MjA2MjI2MjQyN30.4hQCHvlq5fnPxOFly_Eu6BIUSG1Xd1TlRs5r2MNCn1A';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
