283d48430ac392e4d80925483cc87976a534aace
achyuthpamisetty <50010594+achyuthpamisetty@users.noreply.github.com>
2025-05-15 14:39:13 +0530

    Updated AuthPage.tsx

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from "../../context/AuthContext";
 import { Headphones } from 'lucide-react';
 
 const AuthPage: React.FC = () => {
