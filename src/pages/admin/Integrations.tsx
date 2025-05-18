d5bfdfd2e4ac4f4b10d430538853039fd60b1bea
achyuthpamisetty <50010594+achyuthpamisetty@users.noreply.github.com>
2025-05-18 22:34:27 +0530

    Deploy application to Netlify

--- src/pages/admin/Integrations.tsx
+++ src/pages/admin/Integrations.tsx
@@ -19,7 +19,8 @@ const Integrations = () => {
         : 'https://login.salesforce.com';
 
     const authUrl = `${baseUrl}/services/oauth2/authorize?response_type=code&client_id=${encodeURIComponent(
-VITE_SF_CLIENT_ID)}&redirect_uri=${encodeURIComponent(VITE_SF_REDIRECT_URI)}&scope=api%20refresh_token`;
+  SF_CLIENT_ID)}&redirect_uri=${encodeURIComponent(SF_REDIRECT_URI)}&scope=api%20refresh_token`;
+
 
     window.location.href = authUrl;
   };
